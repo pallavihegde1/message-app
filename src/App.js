a67a8a8d4b79f766fb4787e3fabf192d2e2e178c
pallavi-syook <pallavi@syook.com>
2017-11-22 10:40:22 +0530

    import and export default components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import MessageView from './Components/message-view'
 
 class App extends Component {
   render() {
@@ -10,15 +11,7 @@ class App extends Component {
       background: 'yellow'
     }
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro" style={pclass}>
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <MessageView/>
     );
   }
 }
