4260b2f4d7ff9c16c9b8dcd73c69b8fd1350d7c7
pallavi-syook <pallavi@syook.com>
2017-11-24 11:36:40 +0530

    bind this to input feilds

--- src/Components/message_form.js
+++ src/Components/message_form.js
@@ -1,9 +1,23 @@
 import React, {Component} from 'react';
 
 class MessageForm extends Component{
+  constructor(props){
+    super(props);
+
+    this.state = {title: ''};
+  }
+
+  handleChange(event){
+    this.setState({title: event.target.value});
+  }
+
   render() {
     return(
-      <div><h1> Message Form </h1></div>
+      <div>
+        <form>
+          <input type="text" name="content" value={this.state.title} onChange={this.handleChange.bind(this)}/>
+        </form>
+      </div>
 
     );
   }
